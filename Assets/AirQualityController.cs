a57597c30952eff33bc529275f459d1284dfdec8
Erik Coltey <colteye@gmail.com>
2020-11-29 02:18:45 -0500

    initial

---
+++ Assets/AirQualityController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AirQualityController : MonoBehaviour
+{
+    [SerializeField] ParticleSystem AirQualityParticle;
+    [SerializeField] float MaxEmissions = 91;
+    [SerializeField] float particleMaxSize = 100;
+
+    [SerializeField] Text EmissionsText;
+
+    // Assume level goes from 0 to 1.
+    public void ChangeEmissions(float level)
+    {
+        float currentEmissions = level * MaxEmissions;
+        EmissionsText.text = string.Format("Current Emissions: {0} Billion Metric Tons of CO2", currentEmissions);
+
+        var main = AirQualityParticle.main;
+        main.startSize = level * particleMaxSize;
+    }
+}
---
+++ Assets/OrbitCamera.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class OrbitCamera : MonoBehaviour
+{
+    [SerializeField] Transform focus;
+    [SerializeField, Range(1f, 200f)] float distance = 50f;
+    [SerializeField, Min(0f)] float focusRadius = 1f;
+    [SerializeField] float zoomScaler = 0.1f;
+
+    Vector3 focusPoint;
+    float currentZoom = 1;
+
+    void Awake()
+    {
+        focusPoint = focus.position;
+    }
+
+    void LateUpdate()
+    {
+        //Vector3 focusPoint = focus.position;
+        UpdateFocusPoint();
+        Vector3 lookDirection = transform.forward;
+
+        currentZoom += Input.mouseScrollDelta.y * zoomScaler;
+        transform.localPosition = focusPoint - lookDirection * (distance * currentZoom);
+    }
+
+    void UpdateFocusPoint()
+    {
+        Vector3 targetPoint = focus.position;
+        if (focusRadius > 0f)
+        {
+            float distance = Vector3.Distance(targetPoint, focusPoint);
+            if (distance > focusRadius)
+            {
+                focusPoint = Vector3.Lerp(
+                    targetPoint, focusPoint, focusRadius / distance
+                );
+            }
+        }
+        else
+        {
+            focusPoint = targetPoint;
+        }
+    }
+}
